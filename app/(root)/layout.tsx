2672e05f79c80637958a8b8bffbf9b69433c2bfe
ignatius21 <ignaciojosealberto@hotmail.com>
2024-08-16 15:43:21 -0300

    creacion de la homepage e implementacion del darkTheme en clerk

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/Bottombar";
+import Leftsidebar from "@/components/shared/Leftsidebar";
+import Rightsidebar from "@/components/shared/Rightsidebar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main className="flex flex-row">
+            <Leftsidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <Rightsidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+      </ClerkProvider>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,8 @@ import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <ClerkProvider>
-      <div>
-        <UserButton />
-      </div>
-    </ClerkProvider>
+      <>
+        <h1 className="head-text text-left">Home</h1>
+      </>
   );
 }
