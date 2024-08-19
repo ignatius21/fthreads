f61e0c7c6eaca3c02c488e6c8c4bc29ec68d0252
ignatius21 <ignaciojosealberto@hotmail.com>
2024-08-19 20:29:30 -0300

    Agregado de los fetch posts a la pagina principal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
-import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { formatDateString } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+
+
+export default async function Home() {
+  const results = await fetchPosts(1,10);
+  const user = await currentUser();
   return (
-      <>
-        <h1 className="head-text text-left">Home</h1>
-      </>
+    <>
+      <h1 className="head-text">Home</h1>
+    <section className="mt-9 flex flex-col gap-10">
+      {results.posts.length === 0 ? (
+        <p>No post</p>
+      ): (
+        results.posts.map((post) => (
+          <ThreadCard
+            key={post._id}
+            id={post._id}
+            post={post}
+            currentUserId= {user?.id}
+            parentId={post.parentId}
+            content={post.text}
+            author={post.author}
+            community={post.community}
+            createdAt={formatDateString(post.createdAt)}
+            comments={post.children}
+          />
+        ))
+      )}
+    </section>
+    </>
   );
 }
