08dccc183406deb2811957dbedc1bf8c20a722ff
ignatius21 <ignaciojosealberto@hotmail.com>
2024-08-22 20:47:16 -0300

    feat: Update Home page to use ThreadCard component for displaying posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,25 +11,25 @@ export default async function Home() {
   return (
     <>
       <h1 className="head-text">Home</h1>
-    <section className="mt-9 flex flex-col gap-10">
-      {results.posts.length === 0 ? (
-        <p>No post</p>
-      ): (
-        results.posts.map((post) => (
-          <ThreadCard
-            key={post._id}
-            id={post._id}
-            currentUserId= {user?.id || ''}
-            parentId={post.parentId}
-            content={post.text}
-            author={post.author}
-            community={post.community}
-            createdAt={formatDateString(post.createdAt)}
-            comments={post.children}
-          />
-        ))
-      )}
-    </section>
+      <section className="mt-9 flex flex-col gap-10">
+        {results.posts.length === 0 ? (
+          <p>No post</p>
+        ) : (
+          results.posts.map((post) => (
+            <ThreadCard
+              key={post._id}
+              id={post._id}
+              currentUserId={user?.id || ""}
+              parentId={post.parentId}
+              content={post.text}
+              author={post.author}
+              community={post.community}
+              createdAt={formatDateString(post.createdAt)}
+              comments={post.children}
+            />
+          ))
+        )}
+      </section>
     </>
   );
 }
