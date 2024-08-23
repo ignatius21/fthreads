5774de99f1ec9d5dd9b91fd437f2e3cd3c70c09d
ignatius21 <ignaciojosealberto@hotmail.com>
2024-08-23 05:28:59 -0300

    feat: Update Home page to use optional chaining for results.posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,10 +12,10 @@ export default async function Home() {
     <>
       <h1 className="head-text">Home</h1>
       <section className="mt-9 flex flex-col gap-10">
-        {results.posts.length === 0 ? (
+        {results?.posts?.length === 0 ? (
           <p>No post</p>
         ) : (
-          results.posts.map((post) => (
+          results?.posts.map((post) => (
             <ThreadCard
               key={post._id}
               id={post._id}
