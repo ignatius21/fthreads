0bcfca215226597ec4b7d1bc2a575a342a7f3f46
ignatius21 <ignaciojosealberto@hotmail.com>
2024-08-25 21:00:18 -0300

    Update ThreadCard component styling and add createdAt timestamp

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -24,7 +24,7 @@ interface Props{
 
 const ThreadCard = ({id,currentUserId,parentId,content,author,createdAt,comments,isComment}:Props) => {
   return (
-    <article className={`flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7': 'bg-dark-2 p-7'} `}>
+      <article className={`flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7': 'bg-dark-2 p-7'} `}>
         <div className='flex items-start justify-between'>
             <div className='flex w-full flex-1 flex-row gap-4'>
                 <div className='flex flex-col items-center'>
@@ -68,25 +68,25 @@ const ThreadCard = ({id,currentUserId,parentId,content,author,createdAt,comments
                                     className='cursor-pointer object-contain'
                                 />
                             </Link>
-                            <Image
+                            {/* <Image
                                 src='/assets/repost.svg'
                                 alt='repost'
                                 width={24}
                                 height={24}
                                 className='cursor-pointer object-contain'
-                            />
-                            <Image
+                                />
+                                <Image
                                 src='/assets/share.svg'
                                 alt='share'
                                 width={24}
                                 height={24}
                                 className='cursor-pointer object-contain'
-                            />
+                                /> */}
                         </div>
                         {isComment && comments.length > 0 && (
                             <Link
                                 href={`/thread/${id}`}
-                            >
+                                >
                                 <p className='mt-1 text-subtle-medium text-gray-1'>{comments.length}</p>
                             </Link>
                         )}
@@ -94,6 +94,7 @@ const ThreadCard = ({id,currentUserId,parentId,content,author,createdAt,comments
                 </div>
             </div>
         </div>
+        <p className='flex justify-start m-4 text-light-3 text-subtle-medium'>{createdAt}</p>
     </article>
   )
 }
